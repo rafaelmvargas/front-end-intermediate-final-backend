fab4f8035bde3e438d53394d2a865eba4031bea8
Rafael Vargas <rafael.vargas@outlook.com>
2021-11-30 21:34:10 -0500

    done

--- api/recipe.model.js
+++ api/recipe.model.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const RecipeSchema = new mongoose.Schema({
   title: String,
   description: String,
+  instructions: String,
   image: String,
   year: String,
 });
